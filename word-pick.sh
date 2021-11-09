56028036c81f47a2f995104d2ef3f265b56df297
sweshelo <sweshelo@dmats97.xyz>
2021-11-09 17:36:56 +0000

    正規表現パターンを修正

--- word-pick.sh
+++ word-pick.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function pick_words(){
-    cat dict.txt | grep "^${1}[A-z ]*\S" | sort -R | head -n 5 | awk -F'\t' '{print "[\""$1"\",\""$2"\"],"}'| sort -R | head -n 5
+    cat dict.txt | grep "^${1}[A-Za-z ]*\S" | sort -R | head -n 5 | awk -F'\t' '{print "[\""$1"\",\""$2"\"],"}'| sort -R | head -n 5
 }
 
 export -f pick_words
