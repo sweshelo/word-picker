fa959d8f48ef15279ab5c291f64b3b9165789f90
sweshelo <sweshelo@dmats97.xyz>
2021-11-18 12:42:44 +0000

    ファイルの存在チェック

--- server.sh
+++ server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-mkfifo stream
+if [ ! -e stream ]; then
+    mkfifo stream
+fi
+
 while true; do
     nc -l 8080 -w 1 < stream | awk '/HTTP/ {system("./word-pick.sh " $2)}' > stream
 done
